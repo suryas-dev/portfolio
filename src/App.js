8fbdad606dda08b8dae0c3ecbde489a04e8262e8
Surya Sowbhagya <suryast.03@gmail.com>
2024-09-16 10:09:58 -0500

    chore: Update routing in App.js and restyle Contact page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -40,6 +14,7 @@ const App = () => (
     <main>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,20 +2,14 @@ import React from 'react';
 import './Contact.css'; // Import the CSS file
 
 const Contact = () => (
-  <div className="contact-container">
-    <h1>Contact Me</h1>
-    <form className="contact-form">
-      <label htmlFor="name">Name:</label>
-      <input type="text" id="name" name="name" required />
-
-      <label htmlFor="email">Email:</label>
-      <input type="email" id="email" name="email" required />
-
-      <label htmlFor="message">Message:</label>
-      <textarea id="message" name="message" rows="5" required></textarea>
-
-      <button type="submit">Send</button>
-    </form>
+  <div className="layout">
+    <div className="contact">
+      <div className="contact__header--getInTouch">GET IN TOUCH</div>
+      <div className="contact__header">Shoot an email to</div>
+      <div className="contact__info">suryasowbhagya@gmail.com</div>
+      <div className="contact__header">Or ring me on</div>
+      <div className="contact__info">+1-270-791-5969</div>
+    </div>
   </div>
 );
 
